1806db9151b419343caafd7aa4b08ecfa9e889b9
Himanshu Kumar Gupta <ai21btech11012@gmail.com>
2022-09-03 21:37:36 +0530

    commit

--- auth.js
+++ auth.js
@@ -1,73 +1,160 @@
 const express=require('express');
 var router =express.Router();
+const bcrypt=require('bcrypt')
+
+const  Signin  = require('./models/users');
 
 
-const { User } = require('./models/users');
 
-// router.get('/',(req,res)=>{
-//     Note.find({}  ,(err,data)=>{
+// router.post('/',(req,res)=>{
+//     var user=new User({
+//         name:req.body.name,
+//         email:req.body.email,
+//         password:req.body.password 
+//     })
+//     user.save((err,data)=>{
 //         if(!err){
-//             res.send(data)
+//             res.send({message:'success'})
 //         }
 //         else{
-//             console.log(err)
+//             // console.log(err);
+//             res.send({message:'error'})
+
 //         }
 //     })
 // })
-// router.get('/:id',(req,res)=>{
-//     id=req.params.id
-//     Note.find({_id:id}  ,(err,data)=>{
-//         if(!err){
-//             res.send(data)
-//         }
-//         else{
-//             console.log(err)
-//         }
-//     })
-// })
 
-router.post('/',(req,res)=>{
-    var user=new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password 
-    })
-    user.save((err,data)=>{
-        if(!err){
-            res.send({message:'success'})
-        }
-        else{
-            // console.log(err);
-            res.send({message:'error'})
 
+router.post('/signup',async(req,res)=>{
+    try {
+        const pw = req.body.pw;
+        const re_pw = req.body.re_pw;
+
+console.log("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // const hell=async(token)=>{
+        //     Signin.updateOne({email:req.body.email},{
+        //         $set:{
+        //             tokens:tokens.concat({token:await token})
+        //         }
+
+        //     })
+        //     }
+
+
+
+        // const verify=await jwt.verify(token,"himanshukumarguptaiswritingthiscodeforjwt");
+
+        // 
+
+        if (pw === re_pw) {
+            console.log("hello")
+
+            const signindata = new Signin({
+                name: req.body.name,
+                email: req.body.email,
+                pw: pw,
+            })
+            console.log("hello")
+
+            // console.log("hel")
+
+            const token = await signindata.tokencreation();
+            // console.log(token)
+            res.cookie("trakev", token, {
+                expires: new Date(Date.now() + 120000),
+                httpOnly: true
+            })
+            // console.log(cookie)
+
+            signuped = await signindata.save()
+
+            console.log("hello")
+
+
+            // console.log("hello")
+
+            console.log("success insterted");
+            res.status(201).send({message:'success'});
         }
-    })
+
+        else {
+            res.send({message:'password not matching'})
+        }
+
+    } catch (error) { res.send({error:"error"}) }
+    // } catch (error) { res.send("Email or Phone Number already registered") }
+
+
+
 })
-// router.put('/:id',(req,res)=>{
-//     var not={
-//         title:req.body.title,
-//         desc:req.body.desc
-//     }
-//     Note.findByIdAndUpdate(req.params.id,{ $set:not},{new:true},(err,data)=>{
-//         if(!err){
-//             res.send(data)
-//         }
-//         else{
-//             console.log(err);
-//         }
-//     })
-// })
-// router.delete('/:id',(req,res)=>{
+router.post("/signin", async (req, res) => {
+    try {
+            const pwget = await Signin.findOne({ email: req.body.email })
+            data=pwget         
+            console.log("hello")
 
-//     Note.findByIdAndDelete(req.params.id,(err,data)=>{
-//         if(!err){
-//             res.send(data)
-//         }
-//         else{
-//             console.log(err);
-//         }
-//     })
-// })
+        
+            global.id = data._id
+            global.name = data.name
+            pw = data.pw
+            console.log("hello")
+        console.log(req.body.pwc)
+            check = await bcrypt.compare(req.body.pwc,pw)
+            console.log("hii")
+
+            try {
+                const token = await data.tokencreation();
+                res.cookie("trakev", token, {
+                    expires: new Date(Date.now() + 120000),
+                    httpOnly: true
+                })
+            } catch (error) {
+                console.log(error)
+                res.send({error:"error"})
+
+            }
+            
+        
+
+        if (check) {
+                const list = await Signin.find({ dvr_stu: "driver" });
+                combo = {}
+                let str = []
+                let ev = []
+                for (let i = 0; i < list.length; i++) {
+                    if (list[i].ev_name != undefined)
+                        str[i] = list[i].ev_name + ":" + list[i].status + ' at ' + list[i].time
+                    ev[i] = list[i].ev_name
+
+                }
+                console.log("hello")
+
+                global.stat = str
+
+                // middle()
+
+                res.status(201).send({message:"success"})
+        }
+    }
+    catch (error) {
+        res.status(400).send({error:"error"});
+    }
+})
 
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -1,9 +1,66 @@
-const mongoose =require('mongoose')
+// const mongoose =require('mongoose')
 
-let User=mongoose.model('signup',{
-    name:{type:String,required: true},
-    email:{type:String,required: true},
-    password:{type:String,required: true}
+// let User=mongoose.model('signup',{
+//     name:{type:String,required: true},
+//     email:{type:String,required: true},
+//     password:{type:String,required: true}
+// })
+
+// module.exports= {User};
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const signinschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pw: {
+        type: String,
+        required: true
+    },
+
+
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+
+
 })
 
-module.exports= {User};
+signinschema.methods.tokencreation = async function () {
+    try {
+
+        const token = jwt.sign({ email: this.email }, "himanshukumarguptaiswritingthiscodeforjwt", { expiresIn: "10 minutes" });
+
+        this.tokens = this.tokens.concat({ token: token })
+        console.log("hel")
+        await this.save()
+        return token
+    }
+    catch (error) { console.log(error) }
+}
+
+signinschema.pre("save", async function (next) {
+    if(this.isModified("pw")){
+    console.log(this.pw)
+    this.pw = await bcrypt.hash(this.pw, 4);
+    console.log(this.pw)
+    }
+    next();
+})
+
+
+const Sign = new mongoose.model("Signin", signinschema);
+module.exports = Sign
+
